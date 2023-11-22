b38dd0020bce08cb472170b66dc25a43bb9eeeb7
shou692199 <shou692199@gmail.com>
2023-11-22 11:05:14 +0800

    Initial commit for PixelExperience
    Change-Id: I0c3d66851e5fa80c9a11b0229838eb0312f2cbba

--- aosp_pdx203.mk
+++ aosp_pdx203.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 IS_PHONE := true
 
-PRODUCT_NAME := lineage_pdx203
+PRODUCT_NAME := aosp_pdx203
 PRODUCT_DEVICE := pdx203
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_BRAND := Sony
--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     SonyPDX203SystemUIRes \
